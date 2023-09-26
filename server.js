6f9ca5557c810bafed233e9ff2a78f1c3b0ed881
Florian-Techer <techerflorian1@gmail.com>
2023-09-26 18:44:20 +0200

    request the api

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const express = require('express');
+const express = require("express");
+const axios = require('axios');
+require('dotenv').config();
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
+});
+
+app.get("/weather", (req, res) => {
+  const params = {
+    access_key: "ba34761150450e66557c0040a2bba463",
+    query: "New York",
+  };
+
+  axios
+    .get("http://api.weatherstack.com/current", { params })
+    .then((response) => {
+      const apiResponse = response.data;
+      console.log(response)
+      res.send(apiResponse);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.listen(PORT, () => {
